6f5024e11c4460cf561231f8acd3de491c69e866
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-25 15:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let form = document.getElementById('form1')
+	let fName = form.elements['fname'];
+	let lName = form.elements['lname'];
 
+	alert(`${fName.value} ${lName.value}`);
 }
